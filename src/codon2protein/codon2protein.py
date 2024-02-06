4dde1757ee185cd410317c330928a6ffce3b2735
Anna Foix <afoix@ebi.ac.uk>
2024-02-06 13:42:20 +0000

    skeleton of the source code

---
+++ src/codon2protein/codon2protein.py
@@ -0,0 +1,34 @@
+#  Read input file (FASTA) and remove the header
+def read_fasta(fasta_file):
+    """
+    
+    """
+    return('')
+
+# Find start codons in the sequence
+def find_codon_start(codon):
+    """
+    
+    """
+    return('')
+
+# Read codon_table.csv and create a dictionary
+def read_codon_table(codon_table='codon_table.csv'):
+    """
+    
+    """
+    return('')      
+
+# Translate the codon to protein
+def translate(sequence, codon_to_amino):
+    """
+    
+    """
+    return('')
+
+# Tie all together
+def main(fasta_input, fasta_output): 
+    """
+    
+    """
+    return('') 
