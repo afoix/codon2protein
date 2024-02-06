23f29ce71d91318f2d997b8484278e94b386c6c3
Anna Foix <afoix@ebi.ac.uk>
2024-02-06 21:28:27 +0000

    small modifications for make the code run

--- src/codon2protein/codon2protein.py
+++ src/codon2protein/codon2protein.py
@@ -3,62 +3,20 @@ import pandas as pd
 import os
 import re
 
-#  Read input file (FASTA) and remove the header (Nick)
-def read_fasta(fasta_file):
-    # I created a folder on my Desktop called 'codon2protein'
-    # This folder contains the 'human_notch.fasta' file
-    # Depending on the folders you have to change the path
-
-    # Define the path to the FASTA file
-    fasta_file_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'codon2protein', 'human_notch.fasta')
-    
-    # Check if the file exists
-    if os.path.exists(fasta_file_path):
-        # Read the contents of the file
-        with open(fasta_file_path, 'r') as fasta_file:
-            lines = fasta_file.readlines()
-    
-        # Remove the header from the FASTA file
-        sequence_lines = []
-        for line in lines:
-            if not line.startswith('>'):  # Remove lines starting with '>'
-                sequence_lines.append(line.strip())
-    
-        # Concatenate the lines to form the sequence
-        sequence = ''.join(sequence_lines)
-    
-        # Print or use the sequence as needed
-        print(sequence[:100])
-
-else:
-    print("The file does not exist.")
-    
-# Find start codons in the sequence (Daniel)
-def find_start_codon(fasta_string: str, first=False, oof=True) -> list:
-    """identify start codons in the provided fasta string
-
-    Args:
-        fasta_string (str): string of DNA nucleotides
-        first (bool, optional): If false, returns the position of all start codons in the string.
-        If true, returns only the first observation of the start codon. Defaults to False.
-        oof(bool, optional): If true, keeps all found start codons. If false, removes all codons that do not
-        align with the first starting base of the fasta string.
-    Returns:
-        list: List of startcodon positions. An entry in the list represents the position of  the first base of the start codon.
+def read_fasta(fasta_file='human_notch.fasta'):
     """
-    # apply regex search on string to find all start codons
-    scodons=[c.start() for c in re.finditer(pattern="ATG", string = fasta_string)]
+    Extract a DNA sequence from a FASTA file as a string
 
-    # remove out of frames codons if wanted
-    if oof==False:
-        scodons = [s for s in scodons if s%3==0]
+    Arguments:
+      fasta_file: the path to the FASTA file to use
 
-    # remove all but the first start codon
-    if first == True:
-        return [scodons[0]]
-    else:
-        return scodons
-    
+    Returns:
+      A DNA string
+    """
+    # get all but the first line in the file
+    lines = open(fasta_file, 'r').readlines()[1:]
+    # strip lines from trailing '\n', join and return them
+    return ''.join([l.strip() for l in lines])
 
 # Read codon_table.csv and create a dictionary (Anna)
 def read_codon_table(codon_table='codon_table.csv'):
@@ -77,68 +35,101 @@ def read_codon_table(codon_table='codon_table.csv'):
         (dictionary, string:string): dictionary with codons as keys and
             amino acid codes as values.
     """
-    
+
     codon_to_amino_acid = {}
-    
+
     with open(codon_table, 'r') as file:
         codon_reader = csv.DictReader(file)
-        codon_to_amino_acid = {row['Codon']:row['AA.Code'] for row in codon_reader}
-        
+        #codon_to_amino_acid = {row['Codon']:row['AA.Code'] for row in codon_reader}
+        # Turn 'U's into 'T's
+        codon_to_amino_acid = {row['Codon'].replace('U', 'T'):row['AA.Code'] for row in codon_reader}
+
     return codon_to_amino_acid
-      
+
+# Find start codons in the sequence (Daniel)
+def find_start_codon(fasta_string: str, first=False, oof=True) -> list:
+    """identify start codons in the provided fasta string
+
+    Args:
+        fasta_string (str): string of DNA nucleotides
+        first (bool, optional): If false, returns the position of all start codons in the string.
+        If true, returns only the first observation of the start codon. Defaults to False.
+        oof(bool, optional): If true, keeps all found start codons. If false, removes all codons that do not
+        align with the first starting base of the fasta string.
+    Returns:
+        list: List of startcodon positions. An entry in the list represents the position of the first base of the start codon.
+    """
+    # apply regex search on string to find all start codons
+    scodons=[c.start() for c in re.finditer(pattern="ATG", string = fasta_string)]
+
+    # remove out of frames codons if wanted
+    if oof==False:
+        scodons = [s for s in scodons if s%3==0]
+
+    # remove all but the first start codon
+    if first == True:
+        return [scodons[0]]
+    else:
+        return scodons
 
 # Translate the codon to protein (Noah)
-def translate(fasta_file, codon_table):
+def translate(dna_sequence, codon_table):
 
-    """This function translates DNA sequences from FASTA files into all conceivable proteins.
-
+    """This function translates DNA sequences into all conceivable proteins.
     #Parameters:
-    #    fasta_file: FASTA file DNA sequence
+    #    dna_sequence: DNA sequence
     #    codon_table: DNA to codon table
 
     #Returns:
-    #    proteins: list of all conceivable proteins that could be translated from input DNA sequence
+    #    proteins: list of all conceivable proteins that could be translated from input DNA sequence together with their associated start index in the dna sequence
     """
-    
-    sequences = read_fasta(fasta_file)
-    proteins = []
 
-    codon_starts = find_codon_start(sequence)
+    proteins = []
+    codon_starts = find_start_codon(dna_sequence)
 
     for start_codon in codon_starts:
         protein = ''
-        for i in range(start_codon, len(sequence), 3):
-                
-            codon = sequence[i:i + 3]
-            amino_acid = codon_table.get
+        for i in range(start_codon, len(dna_sequence), 3):
+            codon = dna_sequence[i:i + 3]
+            amino_acid = codon_table[codon].upper()
 
             if amino_acid != 'STOP':
                 protein += amino_acid
             else:
-                proteins.append(protein)
+                proteins.append((start_codon, protein))
                 break
 
     return proteins
 
 # Tie all together (Jacqueline)
-def main(fasta_input, fasta_output): 
+def codon2protein(fasta_input, codon_csv, output):
+    """
+    Generate all proteins out of the DNA sequence contained in a FASTA file
+    according to the codon table from a given csv file.
+    Writes the results as csv in a file at the given output path.
+
+    Arguments:
+        fasta_input: the path to a FASTA file containing a DNA sequence
+        codon_csv: the path to a csv file containing a codon table
+        output: the path to the file to write the csv results
+    """
+
+    # read dna sequence from input fasta file
+    dna_sequence = read_fasta(fasta_input)
+    # read codon table from input csv file
+    codon_table = read_codon_table(codon_csv)
+
+    # translate dna to proteins
+    indexed_proteins = translate(dna_sequence, codon_table)
 
     #create empty dataframe for appending to
-    data = {"start_codon_index": [], "translated_peptide": []}
-    fasta_output = pd.DataFrame(data)
+    df = pd.DataFrame(indexed_proteins, columns=['start_codon_index', 'translated_peptide'])
+    #augment df with protein sizes
+    df['translated_peptide_length'] = df['translated_peptide'].apply(len)
     #check structure of df
-    print(fasta_output)
+    print(df)
+    # write output
+    df.to_csv(output, index=False)
 
-    #RUN THE FOLLOWING ONCE
-    # run import fasta
-    # run read codon table
-
-    #RUN THE FOLLOWING (likely in for loop, depends on implementation)
-    # run find start codon (Daniel) -- output is (ideally, list of) start codon indexes
-    # run translate codon to AA (Noah) -- output is (ideally, list of) translated codons
-
-    # append input to dataframe, assuming proteins and codons are lists
-    for i in 0..len(proteins):
-        fasta_output=fasta_output.append({"start_codon_index": [codons[i]], "translated_peptide": [proteins[i]]},ignore_index=True)
-    
-    return(fasta_output) 
+if __name__ == '__main__':
+    codon2protein('human_notch.fasta', 'codon_table.csv', 'human_notch_translated_peptides.csv')
