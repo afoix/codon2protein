e0bf53c9a62aaed74eae4cae0c97887c13c32395
Jackie Toussaint <114661621+qtoussaint@users.noreply.github.com>
2024-02-06 14:40:50 +0000

    basic main function structure

--- src/codon2protein/codon2protein.py
+++ src/codon2protein/codon2protein.py
@@ -1,3 +1,6 @@
+#import pandas for use in creating dataframe
+import pandas as pd
+
 #  Read input file (FASTA) and remove the header (Nick)
 def read_fasta(fasta_file):
     """
@@ -28,7 +31,23 @@ def translate(sequence, codon_to_amino):
 
 # Tie all together (Jacqueline)
 def main(fasta_input, fasta_output): 
-    """
+
+    #create empty dataframe for appending to
+    data = {"start_codon_index": [], "translated_peptide": []}
+    fasta_output = pd.DataFrame(data)
+    #check structure of df
+    print(fasta_output)
+
+    #RUN THE FOLLOWING ONCE
+    # run import fasta
+    # run read codon table
+
+    #RUN THE FOLLOWING (likely in for loop, depends on implementation)
+    # run find start codon (Daniel) -- output is (ideally, list of) start codon indexes
+    # run translate codon to AA (Noah) -- output is (ideally, list of) translated codons
+
+    # append input to dataframe, assuming codon_to_amino and codons are lists
+    for i in 0..len(codon_to_amino):
+        fasta_output=fasta_output.append({"start_codon_index": [codons[i]], "translated_peptide": [codon_to_amino[i]]},ignore_index=True)
     
-    """
-    return('') 
+    return(fasta_output) 
