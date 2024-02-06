9a93695061f4eb5a4859ab430e028b7ddc6e3119
Anna Foix <afoix@ebi.ac.uk>
2024-02-06 13:55:19 +0000

    assigned functions

--- src/codon2protein/codon2protein.py
+++ src/codon2protein/codon2protein.py
@@ -1,32 +1,32 @@
-#  Read input file (FASTA) and remove the header
+#  Read input file (FASTA) and remove the header (Nick)
 def read_fasta(fasta_file):
     """
     
     """
     return('')
 
-# Find start codons in the sequence
+# Find start codons in the sequence (Daniel)
 def find_codon_start(codon):
     """
     
     """
     return('')
 
-# Read codon_table.csv and create a dictionary
+# Read codon_table.csv and create a dictionary (Anna)
 def read_codon_table(codon_table='codon_table.csv'):
     """
     
     """
     return('')      
 
-# Translate the codon to protein
+# Translate the codon to protein (Noah)
 def translate(sequence, codon_to_amino):
     """
     
     """
     return('')
 
-# Tie all together
+# Tie all together (Jacqueline)
 def main(fasta_input, fasta_output): 
     """
     
