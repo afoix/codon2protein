f11639dbbcec1e900386932e962780aef4c1da1f
training <training@ip-172-17-32-106.eu-west-2.compute.internal>
2024-02-06 15:06:08 +0000

    update noah

--- src/codon2protein/codon2protein.py
+++ src/codon2protein/codon2protein.py
@@ -14,17 +14,43 @@ def find_codon_start(codon):
 
 # Read codon_table.csv and create a dictionary (Anna)
 def read_codon_table(codon_table='codon_table.csv'):
-    """
+    """    
     
     """
     return('')      
 
 # Translate the codon to protein (Noah)
-def translate(sequence, codon_to_amino):
+def translate(fasta_file, codon_table):
+
+    """This function translates DNA sequences from FASTA files into all conceivable proteins.
+
+    #Parameters:
+    #    fasta_file: FASTA file DNA sequence
+    #    codon_table: DNA to codon table
+
+    #Returns:
+    #    proteins: list of all conceivable proteins that could be translated from input DNA sequence
     """
     
-    """
-    return('')
+    sequences = read_fasta(fasta_file)
+    proteins = []
+
+    codon_starts = find_codon_start(sequence)
+
+    for start_codon in codon_starts:
+        protein = ''
+        for i in range(start_codon, len(sequence), 3):
+                
+            codon = sequence[i:i + 3]
+            amino_acid = codon_table.get
+
+            if amino_acid != 'STOP':
+                protein += amino_acid
+            else:
+                proteins.append(protein)
+                break
+
+    return proteins
 
 # Tie all together (Jacqueline)
 def main(fasta_input, fasta_output): 
